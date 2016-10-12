57df4086816b0b96528058a193c0b2f8856d5cdf
Mohsen Dastjerdi Zade <me@mehsen.com>
2016-10-12 21:53:46 +0330

    Read token from file

--- irwhois.rb
+++ irwhois.rb
@@ -4,13 +4,15 @@ require 'uri'
 
 unavailabes = Hash.new
 
-token = ''
+file = File.new("token.txt", "r")
+token = file.gets.strip
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
       bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+      puts message.chat.id
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
     else
